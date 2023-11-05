583908ecaca8d0ce235038317d0e9333209ca737
Idle Boy <62786811+itsidleboy@users.noreply.github.com>
2023-11-05 17:45:15 +0530

    Update Tailwind configuration and remove unused files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['node_modules/preline/dist/*.js', "./**/*.{html,js}"],
+  content: ['node_modules/preline/dist/*.js', "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require("kutty"), require('preline/plugin'),],
+  plugins: [require("kutty"), require('preline/plugin'), require('@tailwindcss/typography'),],
 }
 
