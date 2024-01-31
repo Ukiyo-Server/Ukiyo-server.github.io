920cc5f09267baebc9449e35edf85b6fd000a98f
Idle Boy <62786811+itsidleboy@users.noreply.github.com>
2024-02-01 01:04:22 +0530

    Update tailwind.config.js with new color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './ _drafts/**/ *.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html', ,
-    './blog/*.html',
-    './about/*.html',
-    './_posts/*.md',
-    './team/*.html',
     './*.md',
     './*js',
     './**/*.html',
@@ -16,17 +9,49 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          light: "#2563EB", // For lighter primary color
-          DEFAULT: "#2563EB", // Normal primary color
-          dark: "#2563EB", // Used for hover, active, etc.
-        },
-      },
+      /**  colors: {
+          primary: {
+            light: "#2563EB", // For lighter primary color
+            DEFAULT: "#2563EB", // Normal primary color
+            dark: "#2563EB", // Used for hover, active, etc.
+          },
+          secondary: {
+            light: "#05a863",
+            DEFAULT: "#05a863",
+            dark: "#05a863",
+          },
+        }, */
     },
   },
   daisyui: {
     themes: [
+      /**      {
+              light: {
+                ...require("daisyui/src/theming/themes")["light"],
+                "primary": "#00a0ff",
+      
+                "secondary": "#05a863",
+      
+                "accent": "#00bbff",
+      
+                "neutral": "#111719",
+      
+                "base-100": "#fff",
+      
+                "info": "#00afce",
+      
+                "success": "#22c55e",
+      
+                "warning": "#c99400",
+      
+                "error": "#ff5a82",
+              },
+              dark: {
+                ...require("daisyui/src/theming/themes")["dark"],
+                primary: "#00a0ff",
+              },
+            },
+            */
       "cupcake",
       "bumblebee",
       "emerald",
@@ -56,11 +81,13 @@ module.exports = {
       "winter",
       "dim",
       "nord",
-      "sunset"
+      "sunset",
+      "dark",
+      "light"
     ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
-    base: false, // applies background color and foreground color for root element by default
-    styled: false, // include daisyUI colors and design decisions for all components
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
@@ -76,6 +103,6 @@ module.exports = {
   corePlugins: {
     aspectRatio: false,
   },
-  
+
 }
 
